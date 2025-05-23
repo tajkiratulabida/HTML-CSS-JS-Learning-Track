b247717e9c3440a773e7a88845ded45ab73c7eb1
Tajkiratul Abida Ananna <tajkiratulabida@gmail.com>
2025-05-24 00:41:37 +0600

    HTML-CSS-Basic-Poject-1

--- ecommerce-app/frontend/src/components/Hero.jsx
+++ ecommerce-app/frontend/src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                         <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
                     </div>
-                    <h1 className='text-3xl sm:py-3 lg:text 5xl leading-relaxed'>Latest Arrivals</h1>
+                    <h1 className='prata-regular text-3xl sm:py-3 lg:text 5xl leading-relaxed'>Latest Arrivals</h1>
                     <div className='flex items-center gap-2'>
                         <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
@@ -19,7 +19,7 @@ function Hero() {
                 </div>
             </div>
             {/* Hero right side */}
-            <img className='w-full sm:w-c1/2' src={assets.hero_img} alt="" />
+            <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
         </div>
     )
 }
--- ecommerce-app/frontend/src/pages/Home.jsx
+++ ecommerce-app/frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import LatestCollectiion from '../components/LatestCollectiion'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollectiion/>
     </div>
   )
 }
